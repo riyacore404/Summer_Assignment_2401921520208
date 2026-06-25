df811235d8e3d71fbac43dcc94204a57264e9508
Riya Rani <rr1182764@gmail.com>
2026-06-25 23:04:16 +0530

    Week 4 Day 4: Path Sum, Binary Tree Maximum Path Sum, Diameter of Binary Tree

---
+++ Week_4/Day_4/124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    int pathSum(TreeNode* root, int& maxi) {
+        if (!root) return 0;
+
+        int left = max(0, pathSum(root->left, maxi));
+        int right = max(0, pathSum(root->right, maxi));
+        maxi = max(maxi, root->val + left + right);
+
+        return root->val + max(left, right); 
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxi = INT_MIN;
+        pathSum(root, maxi);
+        return maxi;
+    }
+};
---
+++ Week_4/Day_4/543_ Diameter_Of_Binary_Tree.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    
+    int height(TreeNode* root, int& diameter) {
+        if (!root) return 0;
+        int left = height(root->left, diameter);
+        int right = height(root->right, diameter);
+
+        diameter = max(diameter, left + right);
+        return 1 + max(left, right);
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if (!root) return 0;
+        int diameter = 0;
+        height(root, diameter);
+
+        return diameter;
+    }
+};
